c6f85713cc3502afa74489959a8f7988f37a733d
Anuragnanda-5 <Anuragnanda60@gmail.com>
2025-06-15 19:19:57 +0530

    Task 1 and 2

---
+++ Operations.py
@@ -0,0 +1,13 @@
+#Mathematical operations
+a= input('Enter the first number: ')
+b= input('Enter the second number: ')
+a=int(a)
+b=int(b)
+c= a + b
+print('Addition: ',c)
+d= a - b
+print('Substraction: ',d)
+e= a * b
+print('Multiplication: ',e)
+f= a/b
+print('Division: ',f)
